472abc8e6ff26fd5789fe6faeb5fe4c69bb82959
Khizar Qureshi <khizarq313@gmail.com>
2022-09-27 22:18:27 +0530

    update on 27,sept.

--- script.js
+++ script.js
@@ -78,8 +78,10 @@ function createTaskLi(taskText, taskStatus, taskId) {
   taskStatusIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="512" height="512" x="0" y="0" viewBox="0 0 24 24" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><linearGradient xmlns="http://www.w3.org/2000/svg" id="linear-gradient" gradientUnits="userSpaceOnUse" x1="-1.28" x2="19.382" y1="26.729" y2="3.812"><stop offset="0" stop-color="#119990"></stop><stop offset="1" stop-color="#EE82EE"></stop></linearGradient><path xmlns="http://www.w3.org/2000/svg" d="m12 1a11 11 0 1 0 11 11 11.013 11.013 0 0 0 -11-11zm4.707 9.207-5 5a1 1 0 0 1 -1.414 0l-3-3a1 1 0 1 1 1.414-1.414l2.293 2.293 4.293-4.293a1 1 0 0 1 1.414 1.414z" fill="url(#linear-gradient)" data-original="url(#linear-gradient)" class=""></path></g></svg>`;
   if (taskStatus) {
     taskStatusIcon.classList.remove("hidden");
+    taskTitle.classList.add("highlight");
   } else {
     taskStatusIcon.classList.add("hidden");
+    taskTitle.classList.remove("highlight");
   }
 
   deleteBtn.innerHTML = `<span">
@@ -178,7 +180,7 @@ function userInputTaskUpdateHandler() {
 }
 
 function buttonColourHighlight(currentBtnName, sideBtn, sideBtn2) {
-  currentBtnName.style.color = "salmon";
+  currentBtnName.style.color = "#24a0ed";
   sideBtn.style.color = "grey";
   sideBtn2.style.color = "grey";
 }
